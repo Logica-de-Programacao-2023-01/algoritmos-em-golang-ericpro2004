c852c3b956404528ebff3f383f6b5fefa9b56072
ericpro2004 <126572065+ericpro2004@users.noreply.github.com>
2023-04-04 09:34:13 -0300

    Create Repetição8.go

---
+++ Repetição8.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Divisores de um número qualquer.
+	var numE int
+	fmt.Println("Digite um número")
+	fmt.Scanln(&numE)
+
+	fmt.Println("Segue lista de divisores do número selecionado na tabuada: ")
+	for i := 1; i <= i; i++ {
+		if numE%i == 0 {
+			fmt.Println(i, " ")
+		}
+	}
+}
