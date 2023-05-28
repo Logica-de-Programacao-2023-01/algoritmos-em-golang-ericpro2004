b6286e35f8912d6f6cf5f67276bae894391aeee4
ericpro2004 <126572065+ericpro2004@users.noreply.github.com>
2023-05-28 15:17:49 -0300

    Create Seleção8.go

---
+++ Seleção8.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num int
+	var diadaSemana string
+	fmt.Println("Digite um número (inteiro) entre 1 e 7: ")
+	fmt.Scanln(&num)
+	
+	if num == 1 {
+		diadaSemana = "domingo"
+	} else if num == 2 {
+		diadaSemana = "segunda-feira"
+	}else if num == 3 {
+		diadaSemana = "terça-feira"
+	}else if num == 4 {
+		diadaSemana = "quarta-feira"
+	} else if num == 5 {
+		diadaSemana = "quinta-feira"
+	} else if num == 6 {
+		diadaSemana = "sexta-feira"
+	} else if num == 7 {
+		diadaSemana = "sábado"
+	} else {
+		diadaSemana = "Inválido"
+	}
+	fmt.Print("Dia da semana correspondente:", diadaSemana)
+}
