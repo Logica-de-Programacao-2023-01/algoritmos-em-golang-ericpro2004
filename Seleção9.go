8d2da6290736a8f56c4c31fa496b967351dfa1f2
ericpro2004 <126572065+ericpro2004@users.noreply.github.com>
2023-05-28 15:26:20 -0300

    Create Seleção9.go

---
+++ Seleção9.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var num1, num2, num3 float64
+
+	fmt.Println("Digite o primeiro número: ")
+	fmt.Scanln(&num1)
+
+	fmt.Println("Digite o segundo número: ")
+	fmt.Scanln(&num2)
+
+	fmt.Println("Digite o terceiro número: ")
+	fmt.Scanln(&num3)
+
+	nums := []float64{num1, num2, num3}
+	sort.Float64s(nums)
+
+	fmt.Println("Ordem crescente dos números: ", nums)
+}
